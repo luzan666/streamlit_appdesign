9fa3b2935770079279458295f35e12d45ddcd016
MaiMai <47183779+luzan666@users.noreply.github.com>
2024-10-27 11:30:56 +0800

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.markdown("# About Us")
 st.markdown ("""
-Project Scope: 
 
-Objectives: 
+Unlock the power of informed home-buying with our app, designed to provide essential insights into HDB resale prices and government grants.
 
-Data Sources: 
+Key Features:
 
-Features: 
+Comprehensive Resale Price Insights: Access the latest median resale prices for HDB flats, categorized by location, household type, and room configuration. Get a quick, reliable reference to help you navigate the HDB resale market effortlessly. Data sourced from the official HDB website: HDB Resale Prices.
 
+Government Grant Calculator: Understand your eligibility for government grants with ease. Our app simplifies the process by detailing the basic grant available based on household type and flat size, as well as the enhanced grant determined by monthly salary ceilings. Make informed decisions with accurate calculations sourced from reputable housing resources: HDB Grant Eligibility and PropertyGuru Grants Guide.
  """)
 st.sidebar.markdown("# About Us")
