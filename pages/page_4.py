e6429e3200795d8ad3a573de44c4e59b3486fcd9
MaiMai <47183779+luzan666@users.noreply.github.com>
2024-10-27 16:42:32 +0800

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -6,4 +6,10 @@ st.sidebar.markdown("# Methodology")
 st.markdown ("""
 dataflow chart 1:  """)
 
-st.image("pages/Screenshot 2024-10-27 at 3.52.13 PM.png", caption="flowchart_1", use_column_width=True)
+st.image("pages/Flowchart - Page 1.png", caption="flowchart_1", use_column_width=True)
+
+
+st.markdown ("""
+dataflow chart 2:  """)
+
+st.image("pages/Flowchart - Page 2.png", caption="flowchart_2", use_column_width=True)
