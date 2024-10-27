ba6b2853fbb28e778bcab8afec8f6e3f28bba41d
MaiMai <47183779+luzan666@users.noreply.github.com>
2024-10-27 10:12:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd 
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Buying HDB flat in the resale market")
+st.sidebar.markdown ("# Main page")
+
+with st.expander("IMPORTANT NOTICE"):
+    st.write("""
+This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+Always consult with qualified professionals for accurate and personalized advice.
+    """)
