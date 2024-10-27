78c0c5fa6fbb41ce00ae652fa1881c0af182d045
MaiMai <47183779+luzan666@users.noreply.github.com>
2024-10-27 15:55:28 +0800

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -4,43 +4,6 @@ st.markdown("# Methodology")
 st.sidebar.markdown("# Methodology")
 
 st.markdown ("""
-dataflow chart 1: 
-[Start]
-   |
-   v
-[Display Title and Description]
-   |
-   v
-[Load Data]
-   |
-   |---> [File Format: CSV]
-   |
-   v
-[User Input Section]
-   |-----------------------|
-   |                       |
-   v                       v
-[Select Town]        [Select Room Type]
-   |                       |
-   v                       v
-[Process User Input with LLM]
-   |
-   v
-[API Interaction (Optional)]
-   |
-   v
-[Retrieve Price from DataFrame]
-   |
-   v
-[Display Result]
-   |
-   v
-[Handle Edge Cases]
-   |
-   v
-[End]
+dataflow chart 1:  """)
 
-             
-dataflow chart 2:
-
- """)
+st.image("Screenshot", caption="flowchart_1", use_column_width=True)
