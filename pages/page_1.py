06a860bd25d62481f7be46655e38e82ec960b4ed
MaiMai <47183779+luzan666@users.noreply.github.com>
2024-10-27 11:43:12 +0800

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -10,5 +10,11 @@ Key Features:
 Comprehensive Resale Price Insights: Access the latest median resale prices for HDB flats, categorized by location, household type, and room configuration. Get a quick, reliable reference to help you navigate the HDB resale market effortlessly. Data sourced from the official HDB website: HDB Resale Prices.
 
 Government Grant Calculator: Understand your eligibility for government grants with ease. Our app simplifies the process by detailing the basic grant available based on household type and flat size, as well as the enhanced grant determined by monthly salary ceilings. Make informed decisions with accurate calculations sourced from reputable housing resources: HDB Grant Eligibility and PropertyGuru Grants Guide.
+
+Reference from: 
+https://www.hdb.gov.sg/-/media/doc/EAPG-CSC/2Q2024-Median-resale-prices.ashx 
+https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/flat-and-grant-eligibility/couples-and-families/cpf-housing-grants-for-resale-flats-families
+https://www.propertyguru.com.sg/property-guides/how-much-hdb-grant-can-you-receive-12777#resale 
  """)
 st.sidebar.markdown("# About Us")
+
